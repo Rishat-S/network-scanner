543d4e77caeea905ad7365f0ff0003a2562bcbb3
Rishat Suleymanov <spk_irbis@mail.ru>
2021-03-26 18:29:49 +0300

    Refactoring

--- src/main/java/ru/lsz/App.java
+++ src/main/java/ru/lsz/App.java
@@ -6,8 +6,8 @@ public class App {
     public static final int THREAD_COUNT = 25;
 
     public static void main(String[] args) {
+        final int range = 250/THREAD_COUNT;
         for(int i = 1; i < THREAD_COUNT; i++) {
-            final int range = 250/THREAD_COUNT;
             new Thread(new RunSearch(HOSTS, i* range, (i+1)* range)).start();;
         }
     }
